23d5ae4e4dbab67c536d7086479da80d635ca50b
SharQawyCS <sharqawycs@gmail.com>
2024-07-04 09:59:12 +0300

    Bug Fixes

--- Global.h
+++ Global.h
@@ -6,43 +6,33 @@ clsUser CurrentUser = clsUser::Find("", "");
 class Global
 {
 public:
-    static void Clear()
-    {
-        // Function to check the OS and perform an action based on the OS type
+        static void Clear()
+        {
+                // Function to check the OS and perform an action based on the OS type
 #if defined(_WIN32) || defined(_WIN64)
-        // Windows
-        system("cls");
-        // Perform Windows-specific action
+                // Windows
+                system("cls");
 #elif defined(__APPLE__) || defined(__MACH__)
-        // macOS
-        system("clear");
-        // Perform macOS-specific action
+                // macOS
+                system("clear");
 #elif defined(__linux__)
-        // Linux
-        system("clear");
-        // Perform Linux-specific action
+                // Linux
+                system("clear");
 #else
-        std::cout << "Unknown OS" << std::endl;
-        // Perform action for unknown OS
+                std::cout << "Unknown OS" << std::endl;
 #endif
-    }
+        }
 
-    static void Pause()
-    {
+        static void Pause()
+        {
 #if defined(_WIN32) || defined(_WIN64)
-        // Windows
-        system("pause>0"); // Perform Windows-specific action
+                system("pause>0"); // Perform Windows-specific action
 #elif defined(__APPLE__) || defined(__MACH__)
-        // macOS
-        system("read -n 1 -s -p \" Press any key to continue... \"; echo");
-        // Perform macOS-specific action
+                system("read -n 1 -s -p \" Press any key to continue... \"; echo");
 #elif defined(__linux__)
-        // Linux
-        system("read -n 1 -s -p \" Press any key to continue... \"; echo");
-        // Perform Linux-specific action
+                system("read -n 1 -s -p \" Press any key to continue... \"; echo");
 #else
-        std::cout << "Unknown OS" << std::endl;
-        // Perform action for unknown OS
+                std::cout << "Unknown OS" << std::endl;
 #endif
-    }
+        }
 };
